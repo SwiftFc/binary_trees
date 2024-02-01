0b5c0a7ab4cfdd1678fe624f686e7a7ffce5bdb5
SwiftFc <aseiduhenrydade@gmail.com>
2024-02-01 15:29:26 -0800

    advanced tasks

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_levelorder - Traverses a binary tree using level-order raversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+*/
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_t **queue = NULL;
+	size_t front = 0, rear = 0;
+
+	queue = malloc(sizeof(binary_tree_t *));
+
+	if (queue == NULL)
+		return;
+
+	queue[rear++] = (binary_tree_t *)tree;
+
+	while (front < rear)
+	{
+		binary_tree_t *current = queue[front++];
+
+		func(current->n);
+
+		if (current->left != NULL)
+		{
+			queue = realloc(queue, (rear + 1) * sizeof(binary_tree_t *));
+
+			if (queue == NULL)
+				return;
+			queue[rear++] = current->left;
+		}
+
+		if (current->right != NULL)
+		{
+			queue = realloc(queue, (rear + 1) * sizeof(binary_tree_t *));
+			if (queue == NULL)
+				return;
+			queue[rear++] = current->right;
+		}
+	}
+
+	free(queue);
+}
